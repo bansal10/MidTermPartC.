78f155c504f50dee10698b121c62c248be748737
Aman Aman <aman10@sheridancollege.ca>
2024-02-23 17:10:12 -0500

    ekjd

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+import java.util.Scanner;
+
+/**
+ * Aman
+ * tiger shroff
+ * @author aman
+ */
+public class TestUserprofile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter favorite actor: ");
+        String actor = scanner.nextLine();
+        System.out.println(name + ", your profile is created with favorite actor " + actor);
+        scanner.close();
+    }
+}
+
+
